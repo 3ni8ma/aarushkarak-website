eea028684606d705d014069fda98a573b1d9685f
Aarush Karak <worldshaans@gmail.com>
2026-07-19 20:19:27 -0400

    Update portfolio: 2026-07-19 21:00:00 [skip ci]

--- src/components/seo/SEOHead.tsx
+++ src/components/seo/SEOHead.tsx
@@ -1,4 +1,4 @@
-// 2026-07-19 20:30:03
+// 2026-07-19 21:00:00
 import { Helmet } from 'react-helmet-async';
 
 interface SEOHeadProps {
--- src/components/ui/AnimatedCounter.tsx
+++ src/components/ui/AnimatedCounter.tsx
@@ -1,4 +1,4 @@
-// 2026-07-19 20:30:03
+// 2026-07-19 21:00:00
 import { useState, useEffect, useRef } from 'react';
 import { useInView } from 'react-intersection-observer';
 
